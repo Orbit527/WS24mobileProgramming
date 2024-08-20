bdf9d735430056db129a565fb9ac9622d0e7b973
Orbit527 <peterschneider15@gmx.net>
2024-08-20 16:36:28 +0300

    implemented Assignment01

--- Assignment01/App.js
+++ Assignment01/App.js
@@ -4,21 +4,40 @@ import {StyleSheet, Text, View, Button, Alert, TextInput} from 'react-native';
 
 export default function App() {
 
-  const [msg, setMsg] = useState("");
+  const [input1, setInput1] = useState("");
+  const [input2, setInput2] = useState("");
+  const [result, setResult] = useState(0);
 
-  const handlePress = () => {
-    Alert.alert("You typed:", msg);
-    console.log("Hello!");
+  const add = () => {
+    setResult(Number(input1) + Number(input2));
+  }
+
+  const subtract = () => {
+    setResult(Number(input1) - Number(input2));
   }
 
   return (
     <View style={styles.container}>
-      <TextInput
-        placeholder={"Write something here"}
-        onChangeText={text => setMsg(text)}
-        keyboardType={"numeric"}
-      />
-      <Button title="Hello" onPress={handlePress} color={"green"}/>
+      <View style={{flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'flex-end'}}>
+        <Text style={styles.text}>Result: </Text>
+        <Text style={styles.text} placeholder={"Result"}>{result}</Text>
+        <TextInput
+          style={styles.textInputField}
+          placeholder={"Enter first number"}
+          onChangeText={input1 => setInput1(parseInt(input1))}
+          keyboardType={"numeric"}
+        />
+        <TextInput
+          style={styles.textInputField}
+          placeholder={"Enter second number"}
+          onChangeText={input2 => setInput2(parseInt(input2))}
+          keyboardType={"numeric"}
+        />
+      </View>
+      <View style={{flex: 1, flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'space-around'}}>
+        <Button title="Add" onPress={add} color={"blue"}/>
+        <Button title="Subtract" onPress={subtract} color={"blue"}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -28,10 +47,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ffffff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
   text: {
-    color: "blue"
+    fontSize: 20,
+  },
+  textInputField: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   }
 });
